0e1411491e83bad8fef2d86f83b6fdcfcefaf20a
Madman <895905300@qq.com>
2019-01-11 18:22:29 +0800

    添加gzip支持

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    { path: '/404', component: () => import('@/views/404') },
     {
       path: '/',
       name: 'home',
@@ -20,6 +21,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    { path: '*', redirect: '/404' }
   ]
 })
---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="not-found">
+        404 - Not found
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.not-found{
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold
+}
+</style>
