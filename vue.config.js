76a2e7648d96284dae86f362858d710dcd3317f6
Madman <895905300@qq.com>
2019-01-11 16:18:06 +0800

    使用cdn 优化, 修改bootcdn 源

--- vue.config.js
+++ vue.config.js
@@ -32,10 +32,10 @@ const cdn = {
       'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
     ],
     js: [
-      'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
-      'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
-      'https://cdn.jsdelivr.net/npm/vuex@3.0.1/dist/vuex.min.js',
-      'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js',
+      'https://cdn.bootcss.com/vue/2.5.21/vue.min.js',
+      'https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js',
+      'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+      'https://cdn.bootcss.com/axios/0.18.0/axios.min.js',
       'https://unpkg.com/element-ui/lib/index.js',
       'https://cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js',
       'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js'
